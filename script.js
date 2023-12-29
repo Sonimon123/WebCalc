e926316139cab2186b0604b8bfdb1944ab03b911
Sonimon123 <jrkargbo@aol.com>
2023-12-29 14:20:10 -0500

    Created initial functions and variables

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let val1 = 0;
+let operator = "+";
+let val2 = 0;
+
+function operate(expression) {
+    
+}
